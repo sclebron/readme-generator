2303bd32f70aafeb17d8d923b5377cb977529fd6
SLeBron <sophieclebron@gmail.com>
2022-08-24 20:08:42 -0600

    added input questions

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the description?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are installation instructions?',
+        name: 'installation',
+    },
+    {
+        type: 'input'
+        message: 'What is the usage information?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What are contribution guidelines?',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions?',
+        name: 'tests',
+    }
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
